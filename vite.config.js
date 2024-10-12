d669b32c30cf6041af7f598563d60572ee1a55aa
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2024-10-12 01:35:39 -0500

    chore(config): add jsconfig.json for path aliasing
    - Set up path aliases for components, hooks, and utils in jsconfig.json

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@auth': path.resolve(__dirname, './src/hooks/auth'),
+      '@data': path.resolve(__dirname, './src/hooks/data'),
+      '@firestore': path.resolve(__dirname, './src/utils/firestore'),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
