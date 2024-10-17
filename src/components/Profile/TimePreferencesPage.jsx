16b3449ad54206d29f986dfaf4c23a3f23772434
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2024-10-17 17:45:06 -0500

    Revert "adding code so that after the user saves their preferences the app navigates back to the profile page."
    This reverts commit f35e89bdfd1b1c9b85f2b8e998a8c2264f5ee50b.

--- src/components/Profile/TimePreferencesPage.jsx
+++ src/components/Profile/TimePreferencesPage.jsx
@@ -4,44 +4,38 @@ import { useTimePreferences } from '@data/useTimePreferences';
 import { Box, Typography, Button, CircularProgress } from '@mui/material';
 
 import TimePreferencesGrid from './TimePreferencesGrid';
-import { useAuthState } from '@auth/useAuthState';
-import { useNavigate } from "react-router-dom";
 
 export default function TimePreferencesPage() {
-    const [user] = useAuthState();
-    const { selectedTimes, setSelectedTimes, loading, savePreferences } = useTimePreferences();
-    const navigate = useNavigate();
+  const { selectedTimes, setSelectedTimes, loading, savePreferences } = useTimePreferences();
 
-    // Function to save preferences and navigate back to profile page
-    const handleSavePreferences = async () => {
-        await savePreferences();
-        navigate(`/profile/${user.uid}`);
+  // Function to save preferences and navigate back to profile page
+  const handleSavePreferences = async () => {
+    await savePreferences();
+  };
 
-    };
-
-    if (loading) {
-        return (
-            <Box
-                sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}
-            >
-                <CircularProgress />
-            </Box>
-        );
-    }
-
+  if (loading) {
     return (
-        <Box sx={{ maxWidth: 800, margin: 'auto', padding: 3, alignItems: 'center' }}>
-            <Typography variant="h4" align="center" gutterBottom>
-                Time Preferences
-            </Typography>
-
-            <TimePreferencesGrid selectedTimes={selectedTimes} setSelectedTimes={setSelectedTimes} />
-
-            <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 2 }}>
-                <Button variant="contained" onClick={handleSavePreferences}>
-                    Save Preferences
-                </Button>
-            </Box>
-        </Box>
+      <Box
+        sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}
+      >
+        <CircularProgress />
+      </Box>
     );
+  }
+
+  return (
+    <Box sx={{ maxWidth: 800, margin: 'auto', padding: 3, alignItems: 'center' }}>
+      <Typography variant="h4" align="center" gutterBottom>
+        Time Preferences
+      </Typography>
+
+      <TimePreferencesGrid selectedTimes={selectedTimes} setSelectedTimes={setSelectedTimes} />
+
+      <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 2 }}>
+        <Button variant="contained" onClick={handleSavePreferences}>
+          Save Preferences
+        </Button>
+      </Box>
+    </Box>
+  );
 }
