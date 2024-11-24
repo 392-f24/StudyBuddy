03bad719d7e77d8109f6b93223b3eb937613413e
MC2b6 <micaserio606@gmail.com>
2024-11-24 15:22:43 -0600

    Test app launching

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -1,5 +1,9 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+/* globals cy */
+
+describe('Test App', () => {
+
+  it('launches', () => {
+    cy.visit('/');
+  });
+
+});
